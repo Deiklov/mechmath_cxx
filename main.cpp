ad465085efea559957d1c7530dce267add6ed45b
Романов Андрей Владимирович <AVladiRomanov@sberbank.ru>
2021-11-18 08:18:39 +0300

    wrong version

--- main.cpp
+++ main.cpp
@@ -1,98 +1,202 @@
-#include <cassert>
 #include <cstdlib>
-#include <cstring>
 #include <ctime>
 #include <iostream>
-#include <list>
-#include <string>
+#include <iterator>
 #include <utility>
 #include <vector>
 
 using namespace std;
 
-void directSort(vector<string>::iterator first, vector<string>::iterator last);
+void bubbleSort(
+    vector<double>::iterator first,  // Beginning of array segment to sort
+    vector<double>::iterator last    // After-end of array segment to sort
+);
+void directSort(vector<double>::iterator first, vector<double>::iterator last);
 
-void radixSort(vector<string>::iterator first, vector<string>::iterator last,
-               int compareLength = 64);
+void heapSort(vector<double>::iterator first, vector<double>::iterator last);
 
-void printArray(vector<string>::iterator first, vector<string>::iterator last);
+void bubbleDown(vector<double>::iterator first,  // Beginning of array segment
+                size_t i,  // index of element to sieve in the array segment
+                size_t n   // size of the array segment
+);
 
-void generateRandomString(string& s, int minLength = 1, int maxLength = 70);
+void merge(vector<double>::iterator a0,  // Beginning of first array
+           vector<double>::iterator a1,  // After-end of first array
+           vector<double>::iterator b0,  // Beginning of second array
+           vector<double>::iterator b1,  // After-end of second array
+           vector<double>::iterator c    // Beginning of resulting array
+);
 
-int main() {
-  time_t tim;
-  tim = time(&tim);
-  srand(int(tim));
-  vector<string> a;
-  while (true) {
-    int n;
-    cout << "n: ";
-    cin >> n;
-    if (n <= 0) break;
+void mergeSortRecursive(
+    vector<double>::iterator first,     // Beginning of array segment to sort
+    vector<double>::iterator last,      // After-end of array segment to sort
+    std::vector<double>* tmpMemory = 0  // Temporary memory to use
+);
 
-    int minLen = 0, maxLen = 0;
-    cout << "Minimal and maximal lengths of strings: " << endl;
-    cin >> minLen >> maxLen;
-    if (minLen == 0 || maxLen < minLen) break;
+void mergeSortBottomTop(
+    vector<double>::iterator first,     // Beginning of array segment to sort
+    vector<double>::iterator last,      // After-end of array segment to sort
+    std::vector<double>* tmpMemory = 0  // Temporary memory to use
+);
 
-    int sortAlg;
-    cout << "Enter number of sorting algorithm:" << endl;
-    cout << "1 DirectSort, 2 RadixSort" << endl;
-    cin >> sortAlg;
+void printArray(vector<double>::iterator first, vector<double>::iterator last);
 
-    int compareLength = 0;
-    if (sortAlg == 2) {
-      cout << "Enter string length to compare: " << endl;
-      cin >> compareLength;
-      if (compareLength == 0) break;
-    }
+// int main() {
+//   time_t tim;
+//   tim = time(&tim);
+//   srand(int(tim));
+//   vector<double> a;
+//   while (true) {
+//     int n;
+//     cout << "n: ";
+//     cin >> n;
+//     if (n <= 0) break;
+//
+//     cout << "Enter number of sorting algorithm:" << endl;
+//     cout << "1 BubbleSort, 2 DirectSort, 3 HeapSort," << endl;
+//     cout << "4 Recursive merge sort, 5 Bottom-Top merge sort" << endl;
+//     int sortAlg;
+//     cin >> sortAlg;
+//
+//     // Generate a random array of size n
+//     a.resize(n);
+//     for (int i = 0; i < n; ++i) {
+//       double x = (double(rand()) / double(RAND_MAX)) * 1000.;
+//       a[i] = x;
+//     }
+//     cout << "Random array:" << endl;
+//     printArray(a.begin(), a.end());
+//     cout << "-------------" << endl;
+//
+//     if (sortAlg == 1)
+//       bubbleSort(a.begin(), a.end());
+//     else if (sortAlg == 2)
+//       directSort(a.begin(), a.end());
+//     else if (sortAlg == 3)
+//       heapSort(a.begin(), a.end());
+//     else if (sortAlg == 4)
+//       mergeSortRecursive(a.begin(), a.end());
+//     else if (sortAlg == 5)
+//       mergeSortBottomTop(a.begin(), a.end());
+//     else
+//       continue;
+//     cout << "Sorted array:" << endl;
+//     printArray(a.begin(), a.end());
+//     cout << "-------------" << endl;
+//   }
+//   return 0;
+// }
 
-    // Generate a random array of size n
-    a.resize(n);
-    for (int i = 0; i < n; ++i) {
-      generateRandomString(a[i], minLen, maxLen);
-    }
-    cout << "Random array:" << endl;
-    printArray(a.begin(), a.end());
-    cout << "-------------" << endl;
-
-    if (sortAlg == 1)
-      directSort(a.begin(), a.end());
-    else if (sortAlg == 2)
-      radixSort(a.begin(), a.end(), compareLength);
-    else
-      continue;
-    cout << "Sorted array:" << endl;
-    printArray(a.begin(), a.end());
-    cout << "-------------" << endl;
-  }
-  return 0;
-}
-
-void printArray(vector<string>::iterator a,    // Beginning of array segment
-                vector<string>::iterator last  // After-end of array segment
-) {
+void printArray(vector<double>::iterator a, vector<double>::iterator last) {
   size_t n = last - a;
   if (n <= 1000) {
     // Print all array
     for (size_t i = 0; i < n; ++i) {
-      cout << a[i] << endl;
+      if (i > 0) {
+        // Print a delimiter
+        //        if (i % 5 == 0)
+        //          cout << endl;
+        //        else
+        cout << " ";
+      }
+      cout << a[i];
     }
   } else {
     // The array is too big, print its beginning and end only
     // (100 first and 100 last elements)
     for (size_t i = 0; i < 100; ++i) {
-      cout << a[i] << endl;
+      if (i > 0) {
+        // Print a delimiter
+        if (i % 5 == 0)
+          cout << endl;
+        else
+          cout << " ";
+      }
+      cout << a[i];
     }
-    cout << ". . ." << endl;
+    cout << endl << ". . ." << endl;
     for (size_t i = n - 100; i < n; ++i) {
-      cout << a[i] << endl;
+      if (i > n - 100) {
+        // Print a delimiter
+        if (i % 5 == 0)
+          cout << endl;
+        else
+          cout << " ";
+      }
+      cout << a[i];
     }
+  }
+  cout << endl;
+}
+
+int main() {
+  vector<double> arr{1, 12, 53,  -2,  -11, 45,  89, 32,
+                     8, 19, -32, -67, 100, 100, 12, -11};
+  vector<double> tmpMemory(arr.size());
+  //
+  mergeSortRecursive(arr.begin(), arr.end(), &tmpMemory);
+  printArray(tmpMemory.begin(), tmpMemory.end());
+  cout << "kek" << endl;
+  return 0;
+}
+
+void mergeSortRecursive(vector<double>::iterator a,
+                        vector<double>::iterator last,
+                        vector<double>* tmpMemory) {
+  size_t n = distance(a, last);
+  if (n <= 3) {
+    directSort(a, last);
+    return;
+  }
+
+  size_t k = n / 2;
+
+    mergeSortRecursive(a, a+k, tmpMemory);
+    mergeSortRecursive(a+k, last, tmpMemory);
+    ::merge(a, a+k, a+k, last, tmpMemory->begin());
+    cout << "kek" << endl;
+
+}
+
+void merge(vector<double>::iterator a0,  // Beginning of first array
+           vector<double>::iterator a1,  // After-end of first array
+           vector<double>::iterator b0,  // Beginning of second array
+           vector<double>::iterator b1,  // After-end of second array
+           vector<double>::iterator c    // Beginning of resulting array
+) {
+  while (a0 != a1 && b0 != b1) {
+    if (*a0 <= *b0) {
+      *c = *a0;
+      ++a0;
+    } else {
+      *c = *b0;
+      ++b0;
+    }
+    ++c;
+  }
+
+  while (a0 != a1) {
+    *c = *a0;
+    ++a0;
+    ++c;
+  }
+
+  while (b0 != b1) {
+    *c = *b0;
+    ++b0;
+    ++c;
   }
 }
 
-void directSort(vector<string>::iterator a,    // Beginning of array segment
-                vector<string>::iterator last  // After-end of array segment
+void mergeSortBottomTop(vector<double>::iterator first,
+                        vector<double>::iterator last,
+                        std::vector<double>* tmpMemory) {
+  //... to be implemented...
+  cout << "mergeSortBottomTop is not implemented yet..." << endl;
+}
+void directSort(
+    vector<double>::iterator a,    // Beginning of array segment to sort
+    vector<double>::iterator last  // After-end of array segment
 ) {
   size_t n = last - a;
   for (size_t i = 0; i < n - 1; ++i) {
@@ -103,44 +207,3 @@ void directSort(vector<string>::iterator a,    // Beginning of array segment
     if (indMin != i) swap(a[i], a[indMin]);
   }
 }
-
-static const char* const LETTERS = "abcdefghijklmnopqrstuvwxyz";
-// "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
-static const char* const DIGITS = "0123456789";
-
-void generateRandomString(string& s, int minLength /* = 1 */,
-                          int maxLength /* = 70 */
-) {
-  //... int numLetters = int(strlen(LETTERS));
-  int numLetters = 0;
-  int numDigits = int(strlen(DIGITS));
-  s.clear();
-
-  // Select a random length
-  int len = minLength + rand() % (maxLength + 1 - minLength);
-
-  for (int i = 0; i < len; ++i) {
-    int r = rand() % (numLetters + numDigits);
-    char c;
-    if (r < numLetters) {
-      // Add a random letter
-      c = LETTERS[rand() % numLetters];
-    } else {
-      // Add a random digit
-      c = DIGITS[rand() % numDigits];
-    }
-    s.push_back(c);
-  }
-}
-
-void radixSort(vector<string>::iterator first, vector<string>::iterator last,
-               int compareLength /* = 64 */
-) {
-  //... to be implemented...
-  cout << "radixSort is not implemented yet..." << endl;
-}
-void mergeSortRecursive(
-    vector<double>::iterator first, // Beginning of array segment
-    vector<double>::iterator last,  // After-end of array segment
-    vector<double>* tmpMemory = 0   // Temporary memory to use
-);
